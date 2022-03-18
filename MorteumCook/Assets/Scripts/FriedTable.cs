64c47378482aede8d130bd3d3f85a5e771ad33ea
DenevCode <denevdesign@gmail.com>
2022-03-17 19:24:06 -0500

    Enums Functions on the table class for the session

--- MorteumCook/Assets/Scripts/FriedTable.cs
+++ MorteumCook/Assets/Scripts/FriedTable.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class FriedTable : Table
 {
-    
+    //public IngredientType ingredientEnum;
+    //public IngredientType ingredient2;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ValidateIngredient(ingredientEnum);
+        //AddIngredient(ingredientEnum);
+        //AddIngredient(ingredient2);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RemoveIngredient(ingredientEnum);
+            //ValidateRecipe();
+        }
+
         if(isEmpty() == false){
             Ingredient ingredient =  tablePoint.transform.GetChild(0).GetComponent<Ingredient>();            
             ingredient.isCooked = true; 
         }
         
+
+
+
     }
 
     override public bool canUseTable(GameObject other){
--- MorteumCook/Assets/Scripts/Table.cs
+++ MorteumCook/Assets/Scripts/Table.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Table : MonoBehaviour
 {   
     [HideInInspector]public GameObject tablePoint;
+    [HideInInspector] public IngredientType tableIngredientType;
+    public IngredientType receta;
 
-    
-    
+    public IngredientType recetaUI;
+
     void Awake()
     {
         tablePoint = this.transform.GetChild(0).gameObject;
@@ -31,5 +33,46 @@ public class Table : MonoBehaviour
        return true;
     }
 
+
+    virtual public  bool ValidateIngredient(IngredientType ingredient) 
+    {
+        bool validation = false;
+        if (tableIngredientType.HasFlag(ingredient))
+        {
+            Debug.Log("Ingrediente correcto");
+        }
+        else
+        {
+            Debug.Log("No coincide ingrediente");
+        }
+
+        return validation;
+    }
+
+    virtual public bool ValidateRecipe() 
+    {
+        if (recetaUI == receta)
+        {
+            Debug.Log("Receta Correcta");
+        }
+        else
+        {
+            Debug.Log("Receta Incorrecta");
+        }
+        
+        return recetaUI.HasFlag(receta);        
+    }
+
+
+    virtual public void AddIngredient(IngredientType ingredient) 
+    {
+        receta |= ingredient;
+        //IngredientType temporal = receta |= ingredient;
+    }
+
+    virtual public void RemoveIngredient(IngredientType ingredient) 
+    {
+        receta &= ~ingredient;
+    }
 
 }
