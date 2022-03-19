0580292fd59932fd35819de81ad9e95a01559601
Emmanuel SánchezB <100473617+Esan-7@users.noreply.github.com>
2022-03-18 21:15:23 -0500

    UI programmed

--- MorteumCook/Assets/Scripts/MainMenu.cs
+++ MorteumCook/Assets/Scripts/MainMenu.cs
@@ -5,21 +5,57 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject canvasControl;
+    public GameObject canvasMenu;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+
     {
+
+    }
+
+    public void EscenaJuego()
+
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void EscenaControl(string nombreGuia)
+
+    {
+        canvasMenu.SetActive(false);
+        canvasControl.SetActive(true);
+        //SceneManager.LoadScene("EscControl");
+    }
+
+    public void ShowMenu()
+    {
+        
         
     }
 
-
-    public void EscenaJuego()
+    public void Salir()
     {
-     SceneManager.LoadScene("EscGame")
-     }
-}
+        Application.Quit();
+
+    }
+
+ public void Volver()
+
+    {
+        canvasControl.SetActive(false);
+        canvasMenu.SetActive(true);
+        //SceneManager.LoadScene("MainMenuEsb");
+    }
+
+
+    }
