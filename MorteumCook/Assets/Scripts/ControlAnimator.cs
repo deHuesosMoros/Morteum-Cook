50e9f6a4f1562102a0bb3ec1a5968a56c8b19f87
durriago <gotademiel287@gmail.com>
2022-03-22 22:22:30 -0500

    Control Animator

--- MorteumCook/Assets/Scripts/ControlAnimator.cs
+++ MorteumCook/Assets/Scripts/ControlAnimator.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ControlAnimator : MonoBehaviour
 {
+
+    public Animator leerAnimator;
+    public GameObject player;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,36 @@ public class ControlAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            leerAnimator.SetBool("Grab", true);
+            leerAnimator.SetBool("hasGrabbed", true);
+
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            leerAnimator.SetBool("Grab", false);
+            leerAnimator.SetBool("hasGrabbed", false);
+
+        }
+
+
+
+
+
+        if (Input.GetAxis("Horizontal")>0 || Input.GetAxis("Horizontal") < 0 || Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+        {
+            leerAnimator.SetBool("Walking", true);
         
+        }
+        else 
+        {
+            leerAnimator.SetBool("Walking", false);
+
+        }
+
     }
 }
