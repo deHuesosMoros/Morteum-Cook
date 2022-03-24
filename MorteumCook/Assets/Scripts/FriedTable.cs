e4e271f4e643b76acabadfac031272db8857c1cf
Laura Muñoz Caicedo <64536735+lauramunozcaiced@users.noreply.github.com>
2022-03-24 12:14:38 -0500

    testing

--- MorteumCook/Assets/Scripts/FriedTable.cs
+++ MorteumCook/Assets/Scripts/FriedTable.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class FriedTable : Table
 {
-    
+    public int timeCook = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +15,21 @@ public class FriedTable : Table
     // Update is called once per frame
     void Update()
     {
+        
         if(isEmpty() == false){
-            Cooked(tablePoint.transform.GetChild(0).GetComponent<Ingredient>());        
-             
+            timeCook ++;
+            Debug.Log("Está entrando");
+            if(timeCook >= 300){
+                Cooked(tablePoint.transform.GetChild(0).GetComponent<Ingredient>()); 
+                timeCook = 0;
+            } 
+        }else{
+            timeCook = 0;
         }
         
     }
+
+    
 
     
 }
